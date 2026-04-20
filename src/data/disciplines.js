0a647c312030caeb770952fa6ddecee1a4989117
Mydraneth <keremyeles@gmail.com>
2026-04-20 23:51:25 +0300

    added image links to disciplines

--- src/data/disciplines.js
+++ src/data/disciplines.js
@@ -5,6 +5,7 @@ export const disciplines = [
 		description:
 			"Utilized by those who desire a close and supernatural bond with the animal world as well as a vampire's own Beast.",
 		mainClans: ["Gangrel", "Ravnos", "Nosferatu", "Tzimisce"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/5/57/Animalism_symbol.png/1023px-Animalism_symbol.png?20220205151544",
 	},
 	{
 		id: 1,
@@ -12,5 +13,6 @@ export const disciplines = [
 		description:
 			"Enables users to hone their senses both physical and psychic in order to bolster their awareness, perceptions, or even see visions of the future.",
 		mainClans: ["Malkavian", "Toreador", "Tremere", "Salubri", "Hecata"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/9/93/Auspex_symbol.png/1023px-Auspex_symbol.png?20220205151601",
 	},
 ];
