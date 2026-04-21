859e027b7d71c49e5727fbef3b0078147e2c64b5
kerem <keremyeles@gmail.com>
2026-04-21 11:46:25 +0300

    done with disciplines

--- src/data/disciplines.js
+++ src/data/disciplines.js
@@ -15,4 +15,95 @@ export const disciplines = [
 		mainClans: ["Malkavian", "Toreador", "Tremere", "Salubri", "Hecata"],
 		image: "https://vtm.paradoxwikis.com/images/thumb/9/93/Auspex_symbol.png/1023px-Auspex_symbol.png?20220205151601",
 	},
+	{
+		id: 2,
+		name: "Blood Sorcery",
+		description:
+			"A type of blood magic that allows the practitioners to manipulate the blood, mortal or vampiric.",
+		mainClans: ["Banu Haqim", "Tremere"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/8/89/Blood_Sorcery_symbol.png/1024px-Blood_Sorcery_symbol.png?20220205151610",
+	},
+	{
+		id: 3,
+		name: "Celerity",
+		description:
+			"Powers up the movement of the user, enabling them to have unnatural quickness in their movement and supernatural reflexes.",
+		mainClans: ["Banu Haqim", "Brujah", "Toreador"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/2/2a/Celerity_symbol.png/1023px-Celerity_symbol.png?20220205155503",
+	},
+	{
+		id: 4,
+		name: "Dominate",
+		description:
+			"Allows the user to use mind control through eye contact and spoken word as well as manipulate memories of their victims.",
+		mainClans: [
+			"Lasombra",
+			"Malkavian",
+			"Salubri",
+			"Tremere",
+			"Tzimisce",
+			"Ventrue",
+		],
+		image: "https://vtm.paradoxwikis.com/images/thumb/1/1b/Dominate_symbol.png/1023px-Dominate_symbol.png?20220205155523",
+	},
+	{
+		id: 5,
+		name: "Fortitude",
+		description: "Strengthens the user's physical and mental resistance.",
+		mainClans: ["Gangrel", "Hecata", "Salubri", "Ventrue"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/0/06/Fortitude_symbol.png/1023px-Fortitude_symbol.png?20220205155538",
+	},
+	{
+		id: 6,
+		name: "Obfuscate",
+		description:
+			"The art of not being seen even in crowds either through being wholly unseen or by blending in.",
+		mainClans: [
+			"Banu Haqim",
+			"Malkavian",
+			"The Ministry",
+			"Nosferatu",
+			"Ravnos",
+		],
+		image: "https://vtm.paradoxwikis.com/images/thumb/a/a8/Obfuscate_symbol.png/1023px-Obfuscate_symbol.png?20220205155559",
+	},
+	{
+		id: 7,
+		name: "Oblivion",
+		description:
+			"Has two branches: one allows the user to manipulate shadows at will and the other enables necromancy or usage of spirits, though both tap into the Abyss.",
+		mainClans: ["Hecata", "Lasombra"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/4/40/Oblivion_symbol.png/1024px-Oblivion_symbol.png?20220205155617",
+	},
+	{
+		id: 7,
+		name: "Potence",
+		description: "Strengthens the user's physical prowess.",
+		mainClans: ["Brujah", "Nosferatu", "Lasombra"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/a/a8/Potence_symbol.png/1023px-Potence_symbol.png?20220205155712",
+	},
+	{
+		id: 8,
+		name: "Presence",
+		description:
+			"Enables the user to use subtle manipulation, control, and swaying of emotions to guide others towards a goal.",
+		mainClans: ["Brujah", "Ravnos", "Toreador", "The Ministry", " Ventrue"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/0/02/Presence_symbol.png/1023px-Presence_symbol.png?20220205155733",
+	},
+	{
+		id: 9,
+		name: "Protean",
+		description:
+			"Grants the ability to change one's shape, grow claws, meld into the earth, or become fog.",
+		mainClans: ["Gangrel", "The Ministry", "Tzimisce"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/8/8a/Protean_symbol.png/1023px-Protean_symbol.png?20220205155750",
+	},
+	{
+		id: 10,
+		name: "Thin-blood Alchemy",
+		description:
+			"The creation of mixtures of blood, emotions, and other varied ingredients to activate unique effects or even copy other Disciplines.",
+		mainClans: ["Thin-bloods"],
+		image: "https://vtm.paradoxwikis.com/images/thumb/6/6e/Alchemy_symbol.png/1024px-Alchemy_symbol.png?20220205151521",
+	},
 ];
